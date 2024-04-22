1fadd165af2d92879216bac1a96dce6fea1a0556
tommydjarf <tommy@djarf.nu>
2024-04-22 10:35:45 +0200

    again

--- travelplanner/src/app/layout.tsx
+++ travelplanner/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./RootLayout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        // Header component goes here
+
+        <main className={styles.main}>
+          <div className="container">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
